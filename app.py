5e65bd63d1ad3bef25e329981942e288593d5467
Mukul Kumar <mukul0000kumar@gmail.com>
2021-09-11 15:31:04 +0530

    Add Comments

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
+
 @app.route('/', methods=['POST','GET'])
+#This fun function returns link_content which for now is just the link given as input by user derived to this program by POST method
 def fun():
     if request.method == "POST":
         link_content = request.form['link']
@@ -8,14 +10,10 @@ def fun():
 
     else:
         return render_template('index.html')
-
-
+#This function return a list of 55 variable which will be calculated in this or in other function by the help of string link which contains a webpage
 def getLink(link):
     ls =[]
     return ls 
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- cassandra/data.py
+++ cassandra/data.py
@@ -5,18 +5,18 @@ cloud_config= {
         'secure_connect_bundle': 'secure-connect-phishing-detection.zip'
 }
 auth_provider = PlainTextAuthProvider('sonalirajput1088@gmail.com', 'shutupBitxh*8')
-cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider) #connect to cloud using user name, password, cloud_config file
 session = cluster.connect()
 
-row = session.execute("select release_version from system.local").one()
+row = session.execute("select release_version from system.local").one()#selects release_version from system.local on datastax cloud
 if row:
     print(row[0])
 else:
     print("An error occurred.")
 
 
-rows = session.execute("INSERT INTO phishing.user_data (id,link,name) VALUES (4,'xyz.xyz','xyz'); ")
-rows = session.execute('SELECT * FROM phishing.user_data')
+rows = session.execute("INSERT INTO phishing.user_data (id,link,name) VALUES (4,'xyz.xyz','xyz'); ")#Insert data into the table
+rows = session.execute('SELECT * FROM phishing.user_data')#Get data from the table
 
 
 for row in rows:
